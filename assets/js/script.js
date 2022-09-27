6d733f20808de3d31588ebf312aa30548d7800fd
Thiviya Sivasthamparam <vakesensivasthamparam@thiviyas-air.lan>
2022-09-27 19:34:48 -0400

    working on api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
 
 
+
 function getInfo() {
     const newName = document.querySelector("#cityInput");
     const cityName = document.querySelector("#cityName");
     cityName.innerHTML = "--" + newName.value + "--";
     console.log("im inside the function");
+   
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newName.value + "&appid=d15d4d136fa5390a176cc9b75210d7b2&units=metric")
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.list.length; i = i + 8) {
-                let todayWeather = data.list[i];
-                // document.getElementById("day" +(i)+ "Min").innerHTML = ("Min:" +Number(data.list[i].main.temp_main));
-                console.log(todayWeather.dt_txt);
-            }
+    fetch ("http://api.openweathermap.org/geo/1.0/direct?q=" + newName.value + "&limit=1&appid=d15d4d136fa5390a176cc9b75210d7b2")
+        .then (response => response.json())
+        .then (data => {
+            
+            console.log(data);
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            return fetch (`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric&appid=d15d4d136fa5390a176cc9b75210d7b2&`);
+        })
+        .then (response => response.json ())
+        .then (data => {
+            console.log(data);
+           
+            let todayTemp = data.daily[0].temp.day;
+            let todayHumidity = data.daily[0].humidity;
+            let todayWindspeed = data.daily[0].windspeed;
+debugger;
+            console.log(todayTemp, todayHumidity, todayWindspeed);
         });
+
+    // fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newName.value + "&appid=d15d4d136fa5390a176cc9b75210d7b2&units=metric")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         for (let i = 0; i < data.list.length; i = i + 8) {
+    //             let todayWeather = data.list[i];
+    //             // document.getElementById("day" +(i)+ "Min").innerHTML = ("Min:" +Number(data.list[i].main.temp_main));
+    //             console.log(todayWeather.dt_txt);
+    //         }
+    //     });
 }
 
 $("#citySelection").click(getInfo);
